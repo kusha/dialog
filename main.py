38519195d4411552d6d216fee031efc34283c4c1
Mark Birger <birgerm@yandex.ru>
2015-01-20 04:47:57 +0100

    Updated dialogs examples

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ Example of dialog manager usage.
 
 """
 
+
 from dialog import Dialog
 
-import datetime
+import datetime, time
 
 def greetings():
     return "Hello " + collocutor
@@ -31,6 +32,14 @@ def day_of_week():
     days = ['Mon','Tues','Wednes','Thurs','Fri','Satur','Sun']
     return days[datetime.datetime.now().weekday()] + 'day'
 
+def make_coffee(responses):
+    print("routine started")
+    time.sleep(5)
+    responses.put('no sugar')
+
+def make_wo_sugar(responses):
+    time.sleep(4)
+    responses.put(True)
 
 if __name__ == "__main__":
     DLG = Dialog(globals())

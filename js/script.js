1325c0eed96fe0149a48b08433f75ae1e67ff90f
Mark Birger <birgerm@yandex.ru>
2015-02-20 04:01:44 +0100

    Deploy fix

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
     var dialogDiv = $("#dialog-interface .panel");
     dialogDiv.animate({ scrollTop: dialogDiv.prop("scrollHeight") - dialogDiv.height() }, 500);
   }
-  var socket =  new WebSocket("ws://178.62.192.111/api");
+  var socket =  new WebSocket("ws://178.62.192.111:8888/api");
   socket.onopen = function() { 
     dialog_show("Connected to the server", "status", "");
     var data = {

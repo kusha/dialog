89c50f3337114a201028736804b0aae664ea2f9b
Mark Birger <birgerm@yandex.ru>
2015-02-20 03:15:53 +0100

    Updated server with reports

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from returns import Returns
 import link_parser
 
 import multiprocessing
+import json
 
 from dialog import Dialog
 
@@ -43,13 +44,13 @@ class Instance(Dialog):
             answers = self.returns.get_returns()
             for answer in answers:
                 tosay, questions = answer.accept()
-                to_user.put(tosay)
+                to_user.put(json.dumps({'type': 'phrase', 'text': tosay}))
                 self._extend_expected(questions)
             # process input
             if not to_ds.empty():
-                input_phrase = to_ds.get()
+                input_origin = to_ds.get()
                 # input_phrase = listener_queue.get() # IN CASE OF SPEECH RECOGNITION
-                input_phrase = link_parser.parse(input_phrase)
+                input_phrase = link_parser.parse(input_origin)
                 states_probability = []
                 for state in self.expected:
                     # print(state, state.compare(input_phrase))
@@ -62,24 +63,38 @@ class Instance(Dialog):
                 state = states_probability[0][0]
                 if states_probability[0][1] < 0.2:
                     # print("Bot> ???")
-                    to_user.put("no answer")
+                    to_user.put(json.dumps({
+                        'type': 'origin',
+                        'text': input_origin
+                    }))
+                    to_user.put(json.dumps({'type': 'unknown'}))
+                    #TODO: save unknown request, with state!!!
                 else:
+                    to_user.put(json.dumps({
+                        'type': 'interpretation',
+                        'origin': input_origin,
+                        'phrase': str(state),
+                        'similarity': states_probability[0][1]
+                    }))
                     tosay, questions = state.accept(input_phrase)
                     for answer in tosay:
                         if answer != "":
                             # speaker_queue.put(answer)
                             # print("Bot> "+answer)
-                            to_user.put("answer: "+answer)
+                            to_user.put(json.dumps({'type': 'phrase', 'text': answer}))
                     self._extend_expected(questions)
 
 from main_slave import export
+
+py_filename = "main_slave.py"
+ddl_filename = "examples/demo.dlg"
 
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
-import time
+import time, os.path
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -94,7 +109,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         dialog = Instance(export)
         # self.write_message("Using dialog description from %s" \
         #     % (dialog_description))
-        dialog.load("examples/tickets.dlg")
+        dialog.load(ddl_filename)
         self.to_user = multiprocessing.Queue(maxsize=0)
         self.to_ds = multiprocessing.Queue(maxsize=0)
         self.dialog_system = multiprocessing.Process(
@@ -107,7 +122,36 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.producer_process.start()
       
     def on_message(self, message):
-        self.to_ds.put(message)
+        message = json.loads(message)
+        if message['type'] == 'phrase':
+            print(">", message)
+            self.to_ds.put(message['text'])
+        elif message['type'] == 'sources':
+            global py_file
+            global ddl_file
+            ddl_modified = int(os.path.getmtime(ddl_filename))
+            py_modified = int(os.path.getmtime(py_filename))
+            ddl_content = open(ddl_filename, "rb").read().decode("utf-8")
+            py_content = open(py_filename, "rb").read().decode("utf-8")
+            response = {
+                "type": "sources",
+                "modified": max(py_modified, ddl_modified) * 1000,
+                "description": {
+                    "filename": ddl_filename,
+                    "content":  ddl_content,
+                    "modified": ddl_modified * 1000
+                },
+                "code": {
+                    "filename": py_filename,
+                    "content":  py_content,
+                    "modified": py_modified * 1000
+                }
+            }
+            self.to_user.put(json.dumps(response))
+        elif message['type'] == 'interpretation':
+            print(">", message)
+            # print(message['origin'])
+            #TODO: save reports
 
     def on_close(self):
         self.producer_process.terminate()
@@ -116,7 +160,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     def producer(self):
         while True:
             message = self.to_user.get()
-            time.sleep(1)
+            time.sleep(0.5)
+            print("<", message)
             self.write_message(message)
 
 # class MainHandler(tornado.web.RequestHandler):

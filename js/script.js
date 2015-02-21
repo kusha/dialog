be8b9b23cf3a5fb7e4820f22ac02ea1b54447c76
Mark Birger <birgerm@yandex.ru>
2015-02-21 12:42:52 +0100

    Graphs scratch

--- js/script.js
+++ js/script.js
@@ -120,5 +120,36 @@ $(document).ready(function() {
     $(this).removeClass("text-danger ipt-report").addClass("text-success").html("reported");
   });
 
+  var ctx = document.getElementById("chart-similarity").getContext("2d");
+  var data = {
+      labels: [],
+      datasets: [
+          // {
+          //     label: "My First dataset",
+          //     fillColor: "rgba(220,220,220,0.2)",
+          //     strokeColor: "rgba(220,220,220,1)",
+          //     pointColor: "rgba(220,220,220,1)",
+          //     pointStrokeColor: "#fff",
+          //     pointHighlightFill: "#fff",
+          //     pointHighlightStroke: "rgba(220,220,220,1)",
+          //     data: [65, 59, 80, 81, 56, 55, 40]
+          // },
+          {
+              label: "My Second dataset",
+              fillColor: "rgba(151,187,205,0.2)",
+              strokeColor: "rgba(151,187,205,1)",
+              pointColor: "rgba(151,187,205,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(151,187,205,1)",
+              data: [28, 48, 40, 19, 30, 27, 40]
+          }
+      ]
+  };
+  for (var i = 20; i <= 100; i+=5) {
+      data.labels.push(i);
+  }
+  var myLineChart = new Chart(ctx).Bar(data);
+
 });
 
